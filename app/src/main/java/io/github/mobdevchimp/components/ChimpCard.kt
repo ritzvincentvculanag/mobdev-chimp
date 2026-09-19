f9e14a2d66fd905340a6c36307c902bbacf4bcc9
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-20 06:43:56 +0800

    add ChimpCardContainer and ChimpCardAnalytics composables

--- app/src/main/java/io/github/mobdevchimp/components/ChimpCard.kt
+++ app/src/main/java/io/github/mobdevchimp/components/ChimpCard.kt
@@ -1,16 +1,92 @@
 package io.github.mobdevchimp.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.github.mobdevchimp.ui.theme.MobdevchimpTheme
+import io.github.mobdevchimp.ui.theme.eelBlack100
+import io.github.mobdevchimp.ui.theme.macawBlue500
 
 @Composable
-fun ChimpCard(modifier: Modifier = Modifier) {
+fun ChimpCardAnalytics(
+    data: Number,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    ChimpCardContainer(modifier = modifier) {
+        Text(
+            text = data.toString(),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = macawBlue500
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = title.uppercase(),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = eelBlack100
+        )
+    }
+}
 
+@Composable
+fun ChimpCardContainer(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .border(
+                width = 2.dp,
+                color = Color(0xE5E7EBFF),
+                shape = RoundedCornerShape(12.dp)
+            )
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            content()
+        }
+    }
 }
 
 @Preview
 @Composable
-private fun ChimpCardPreview() {
-
+private fun ChimpCardAnalyticsContainerPreview() {
+    MobdevchimpTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPaddings ->
+            ChimpCardAnalytics(
+                data = 28,
+                title = "Day Streak",
+                modifier = Modifier
+                    .padding(innerPaddings)
+                    .padding(16.dp)
+            )
+        }
+    }
 }
