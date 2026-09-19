ae3d5eecdfc098c67ae8827b5d48e763ec4253f5
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-20 04:46:03 +0800

    add typography

--- app/src/main/java/io/github/mobdevchimp/ui/theme/Type.kt
+++ app/src/main/java/io/github/mobdevchimp/ui/theme/Type.kt
@@ -2,33 +2,90 @@ package io.github.mobdevchimp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import io.github.mobdevchimp.R
 
-// Set of Material typography styles to start with
+private val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+private val nunito = GoogleFont("Nunito")
+
+val NunitoFontFamily = FontFamily(
+    Font(googleFont = nunito, fontProvider = provider, weight = FontWeight.Light),
+    Font(googleFont = nunito, fontProvider = provider, weight = FontWeight.Normal),
+    Font(googleFont = nunito, fontProvider = provider, weight = FontWeight.Medium),
+    Font(googleFont = nunito, fontProvider = provider, weight = FontWeight.SemiBold),
+    Font(googleFont = nunito, fontProvider = provider, weight = FontWeight.Bold),
+    Font(googleFont = nunito, fontProvider = provider, weight = FontWeight.ExtraBold)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    displayLarge = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 34.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayMedium = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 30.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = NunitoFontFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 20.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
-    */
 )
