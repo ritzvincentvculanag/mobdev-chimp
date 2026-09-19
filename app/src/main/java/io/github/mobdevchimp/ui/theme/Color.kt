48f8ab16a52f0742f92dbe3a19d47ff05673c51e
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-20 04:45:51 +0800

    add colors

--- app/src/main/java/io/github/mobdevchimp/ui/theme/Color.kt
+++ app/src/main/java/io/github/mobdevchimp/ui/theme/Color.kt
@@ -2,10 +2,56 @@ package io.github.mobdevchimp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val featherGreen100: Color = Color(0xFFEDFBE9)
+val featherGreen200: Color = Color(0xFF7EC476)
+val featherGreen300: Color = Color(0xFF50A130)
+val featherGreen500: Color = Color(0xFF0E8A00)
+val featherGreen700: Color = Color(0xFF004E00)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val cardinalRed100: Color = Color(0xFFFBCECE)
+val cardinalRed200: Color = Color(0xFFFF9999)
+val cardinalRed300: Color = Color(0xFFF65555)
+val cardinalRed500: Color = Color(0xFFED0C0C)
+val cardinalRed700: Color = Color(0xFFAA0909)
+
+val macawBlue100: Color = Color(0xFFEAF7FB)
+val macawBlue200: Color = Color(0xFF99D0E5)
+val macawBlue300: Color = Color(0xFF0096D6)
+val macawBlue500: Color = Color(0xFF007DB2)
+val macawBlue700: Color = Color(0xFF003964)
+
+val beeYellow100: Color = Color(0xFFFFF6D7)
+val beeYellow200: Color = Color(0xFFFFEBA3)
+val beeYellow300: Color = Color(0xFFFFDB66)
+val beeYellow400: Color = Color(0xFFFFC200)
+val beeYellow500: Color = Color(0xFFC18101)
+
+val beetlePurple100: Color = Color(0xFFEED5FF)
+val beetlePurple200: Color = Color(0xFFDE99FF)
+val beetlePurple300: Color = Color(0xFFCA66FF)
+val beetlePurple500: Color = Color(0xFFAB2EFF)
+val beetlePurple700: Color = Color(0xFF7A0F9C)
+
+val beakBrown100: Color = Color(0xFFFAD59E)
+val beakBrown200: Color = Color(0xFFECB569)
+val beakBrown300: Color = Color(0xFFD59245)
+val beakBrown500: Color = Color(0xFFB66E28)
+val beakBrown700: Color = Color(0xFF64410D)
+
+val xCyan100: Color = Color(0xFFC9E8D9)
+val xCyan200: Color = Color(0xFF88D694)
+val xCyan300: Color = Color(0xFF44B27D)
+val xCyan500: Color = Color(0xFF06846D)
+val xCyan700: Color = Color(0xFF004A44)
+
+val swanGray100: Color = Color(0xFFF7F7F7)
+val swanGray200: Color = Color(0xFFE5E5E5)
+val swanGray300: Color = Color(0xFFD0D0D0)
+
+val wolfGray100: Color = Color(0xFF8F8F8F)
+val wolfGray200: Color = Color(0xFF777777)
+val wolfGray300: Color = Color(0xFF656565)
+
+val eelBlack100: Color = Color(0xFF606060)
+val eelBlack200: Color = Color(0xFF4B4B4B)
+val eelBlack300: Color = Color(0xFF1A1A1A)
