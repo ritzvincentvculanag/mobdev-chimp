8734d2d44394aa25c11c1ac3c88d1426ec14e46e
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-20 05:28:54 +0800

    add initial code

--- app/src/main/java/io/github/mobdevchimp/ui/screen/GetStartedScreen.kt
+++ app/src/main/java/io/github/mobdevchimp/ui/screen/GetStartedScreen.kt
@@ -1,2 +1,16 @@
 package io.github.mobdevchimp.ui.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun GetStartedScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun GetStartedScreenPreview() {
+
+}
