81dd9cc3e2a02353c6313e6dc6eaa4c389216e90
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-20 03:20:33 +0800

    remove initial code

--- app/src/main/java/io/github/mobdevchimp/MainActivity.kt
+++ app/src/main/java/io/github/mobdevchimp/MainActivity.kt
@@ -19,29 +19,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MobdevchimpTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     MobdevchimpTheme {
-        Greeting("Android")
+
     }
 }
