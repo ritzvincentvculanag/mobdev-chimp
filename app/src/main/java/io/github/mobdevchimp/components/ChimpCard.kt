bb478e0a4999244c815aaa7e63548c8204a4464f
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-20 06:18:14 +0800

    add initial code

--- app/src/main/java/io/github/mobdevchimp/components/ChimpCard.kt
+++ app/src/main/java/io/github/mobdevchimp/components/ChimpCard.kt
@@ -1,2 +1,16 @@
 package io.github.mobdevchimp.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ChimpCard(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun ChimpCardPreview() {
+
+}
