c5e62147cf9c52f2e119b78d390d0de183b7761f
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-20 04:46:19 +0800

    update theme

--- app/src/main/java/io/github/mobdevchimp/ui/theme/Theme.kt
+++ app/src/main/java/io/github/mobdevchimp/ui/theme/Theme.kt
@@ -9,28 +9,37 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = macawBlue500,
+    onPrimary = Color.White,
+    secondary = featherGreen500,
+    onSecondary = Color.White,
+    tertiary = beeYellow400,
+    onTertiary = featherGreen700,
+    background = featherGreen700,
+    onBackground = Color.White,
+    surface = featherGreen700,
+    onSurface = Color.White,
+    error = cardinalRed500,
+    onError = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = macawBlue300,
     onPrimary = Color.White,
+    secondary = featherGreen500,
     onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    tertiary = beeYellow400,
+    onTertiary = featherGreen700,
+    background = Color(0xFFF8FFF8),
+    onBackground = featherGreen700,
+    surface = Color.White,
+    onSurface = featherGreen700,
+    error = cardinalRed500,
+    onError = Color.White
 )
 
 @Composable
@@ -40,15 +49,7 @@ fun MobdevchimpTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 
     MaterialTheme(
         colorScheme = colorScheme,
