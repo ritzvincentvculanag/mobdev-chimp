01e7170a3da4e0db26b6cd609b343df9669f948d
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-20 05:15:48 +0800

    add Google Icons dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.compose.material:material-icons-extended")
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
