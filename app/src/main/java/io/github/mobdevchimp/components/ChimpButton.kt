83946f9b271b1ce9d74a2de1b0e5462a2247190c
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-20 05:12:20 +0800

    add initial button component

---
+++ app/src/main/java/io/github/mobdevchimp/components/ChimpButton.kt
@@ -0,0 +1,106 @@
+package io.github.mobdevchimp.components
+
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.github.mobdevchimp.ui.theme.MobdevchimpTheme
+import io.github.mobdevchimp.ui.theme.macawBlue300
+import io.github.mobdevchimp.ui.theme.macawBlue500
+
+@Composable
+fun ChimpButton(
+    text: String, radius: Dp = 12.dp,
+    foreground: Color = macawBlue300,
+    background: Color = macawBlue500,
+    textColor: Color = Color.White,
+    onClick: () -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val currentOffset by animateDpAsState(
+        label = "buttonOffset",
+        targetValue = if (isPressed) 4.dp else 0.dp
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp + 4.dp)
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = onClick
+            ),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        ChimpButtonContainer(
+            color = background,
+            radius = radius,
+            modifier = Modifier.padding(top = 4.dp)
+        )
+        ChimpButtonContainer(
+            color = foreground,
+            radius = radius,
+            modifier = Modifier.offset(y = currentOffset)
+        ) {
+            Text(
+                text = text.uppercase(),
+                textAlign = TextAlign.Center,
+                letterSpacing = 2.sp,
+                fontFamily = MaterialTheme.typography.labelMedium.fontFamily,
+                fontWeight = MaterialTheme.typography.labelMedium.fontWeight,
+                color = textColor
+            )
+        }
+    }
+}
+
+@Composable
+private fun ChimpButtonContainer(
+    color: Color,
+    radius: Dp,
+    modifier: Modifier = Modifier,
+    buttonText: @Composable () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .background(
+                color = color,
+                shape = RoundedCornerShape(radius)
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        buttonText()
+    }
+}
+
+@Preview
+@Composable
+private fun ChimpButtonPreview() {
+    MobdevchimpTheme {
+        ChimpButton(text = "Hello", onClick = {})
+    }
+}
