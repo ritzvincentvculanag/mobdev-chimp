691292d642baa032b67724e39d2e5650f3f287be
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-21 02:44:06 +0800

    test ChimpCardAnalytics

--- app/src/main/java/io/github/mobdevchimp/MainActivity.kt
+++ app/src/main/java/io/github/mobdevchimp/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.magnifier
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.github.mobdevchimp.components.ChimpButton
+import io.github.mobdevchimp.components.ChimpCardAnalytics
+import io.github.mobdevchimp.ui.screen.GetStartedScreen
 import io.github.mobdevchimp.ui.theme.MobdevchimpTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,16 +28,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MobdevchimpTheme {
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPaddings ->
+                    ChimpCardAnalytics(
+                        data = 28,
+                        title = "Day Streak",
+                        modifier = Modifier
+                            .padding(innerPaddings)
+                            .padding(16.dp)
+                    )
+                }
             }
         }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    MobdevchimpTheme {
-
     }
 }
