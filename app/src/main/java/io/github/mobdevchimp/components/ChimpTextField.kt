36a2e40d295f0e5eb21dc47776bd9968e2b1f69a
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-21 04:02:38 +0800

    add parameters for component

--- app/src/main/java/io/github/mobdevchimp/components/ChimpTextField.kt
+++ app/src/main/java/io/github/mobdevchimp/components/ChimpTextField.kt
@@ -1,29 +1,48 @@
 package io.github.mobdevchimp.components
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import io.github.mobdevchimp.ui.theme.MobdevchimpTheme
+import io.github.mobdevchimp.ui.theme.macawBlue300
+import io.github.mobdevchimp.ui.theme.macawBlue500
+import io.github.mobdevchimp.ui.theme.wolfGray100
+import io.github.mobdevchimp.ui.theme.wolfGray300
+
 
 @Composable
 fun ChimpTextField(
     value: String,
-    onValueChange: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier,
     placeholder: String = "",
-    isError: Boolean = false,
-    modifier: Modifier = Modifier,
-    keyboardOptions: KeyboardOptions,
+    unfocusedBorderColor: Color = wolfGray100,
+    unfocusedShadowColor: Color = wolfGray100,
+    focusedBorderColor: Color = macawBlue300,
+    focusedShadowColor: Color = macawBlue500,
+    backgroundColor: Color = Color.White,
+    textColor: Color = wolfGray300,
+    placeholderColor: Color = wolfGray100,
+    borderWidth: Dp = 2.dp,
+    fontSize: TextUnit = MaterialTheme.typography.bodySmall.fontSize,
     cornerRadius: Dp = 12.dp,
+    offset: Dp = 4.dp,
+    verticalPadding: Dp = 16.dp,
+    horizontalPadding: Dp = 16.dp,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = true
 ) {
 
 }
