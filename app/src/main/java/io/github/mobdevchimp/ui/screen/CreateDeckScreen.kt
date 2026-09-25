0206180dbdc703fee77d13f726f0df9be61915a9
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-26 04:37:48 +0800

    add: test code for navigation

--- app/src/main/java/io/github/mobdevchimp/ui/screen/CreateDeckScreen.kt
+++ app/src/main/java/io/github/mobdevchimp/ui/screen/CreateDeckScreen.kt
@@ -1,2 +1,14 @@
 package io.github.mobdevchimp.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+
+@Composable
+fun CreateDeckScreen() {
+    Column(modifier = Modifier.systemBarsPadding().background(color = Color.Yellow, shape = RectangleShape)) { }
+}
