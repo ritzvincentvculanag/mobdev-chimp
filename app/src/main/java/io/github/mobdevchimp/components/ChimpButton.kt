5fa2c607f7d09bd48172126bc060f0702e9d0e29
Ritz Vincent Culanag <ritzculanagbsit@gmail.com>
2026-09-26 04:28:43 +0800

    update: optimize imports

--- app/src/main/java/io/github/mobdevchimp/components/ChimpButton.kt
+++ app/src/main/java/io/github/mobdevchimp/components/ChimpButton.kt
@@ -11,12 +11,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
